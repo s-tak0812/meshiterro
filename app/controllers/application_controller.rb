365b9ea7c6df53e5ac916a6ecc4765e4d7868f96
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-02-06 09:26:25 +0000

    [Finish]me-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # サインイン後の遷移先指定 gem:deviseメソッド(after_sign_in_path_for)
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  # サインアウト後の遷移先指定 gem:deviseメソッド(after_sign_out_path_for)
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
 
   protected
   # nameカラムの保存許可
