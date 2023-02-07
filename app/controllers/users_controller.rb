ea9e4a58a99dbbcfd10d3af5975f4fbca4d8f06f
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-02-07 10:05:29 +0000

    [Finish]me-16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,22 @@ class UsersController < ApplicationController
     @post_images = @user.post_images
   end
 
-  def edit
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(user_session)
   end
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
 end
