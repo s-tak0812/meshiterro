331bb9e7dc51d997e24be1c9a240815d18e367ae
s-tak0812 <122010689+s-tak0812@users.noreply.github.com>
2023-02-08 17:50:07 +0000

    [Finish]me-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
+  # ユーザー識別
+  before_action :is_matching_login_user, only: [:update, :edit]
+
+
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
   end
 
   def update
+
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(user_session)
@@ -12,6 +18,7 @@ class UsersController < ApplicationController
 
 
   def edit
+
     @user = User.find(params[:id])
   end
 
@@ -21,5 +28,16 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+  # Userコントロール内でのみ使用
+  # ユーザー識別
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+
+
 
 end
